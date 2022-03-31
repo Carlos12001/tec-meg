86a1f721c93c8227f42f3d3ff1aefdbf24b6679c
Carlos12001 <carlos.andres12001@gmail.com>
2022-03-31 13:44:52 -0600

    GUI initials configs.
    The initial config ws finished.

--- main.cpp
+++ main.cpp
@@ -1,34 +1,37 @@
 #include <iostream>
 #include "connection.h"
-class InitProgram{
+#include "mainwindow.h"
+#include <QApplication>
+
+class InitProgram {
 public:
-    static void start(){
+    static void start() {
         return;
     }
-    static void testConnection(){
+
+    static void testConnection() {
         cout << "Welcome to testConnection" << endl;
         int input = 0;
         string a;
         cin >> a;
         input = stoi(a);
-        if(input==0) {
+        if (input == 0) {
             cout << "Test Server" << endl;
             InitProgram::testServerConnection();
-        }
-        else{
+        } else {
             cout << "Test Client" << endl;
             InitProgram::testClientConnection();
         }
     }
-    static void testServerConnection(){
+
+    static void testServerConnection() {
         auto connection = new ServerConnection;
         connection->initConnection();
         cout << connection->getMessage() << endl;
         connection->sendMessage("I got your message");
         delete connection;
         return;
-    };
-
+    }
     static void testClientConnection(){
         auto connection = new ClientConnection;
         connection->sendMessage("I got your message");
@@ -38,8 +41,15 @@ public:
         return;
     };
 
+    static int initGUITest(int argc, char *argv[]){
+        QApplication a(argc, argv);
+        MainWindow w;
+        w.show();
+        return a.exec();
+
+    }
 };
 
-int main() {
-    InitProgram::testConnection();
+int main(int argc, char *argv[]){
+    InitProgram::initGUITest(argc, argv);
 }
