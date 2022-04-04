51735a9245d9a877cbc90eee3777e3ea673ce4ab
Carlos12001 <carlos.andres12001@gmail.com>
2022-04-04 13:39:46 -0600

    New files to project.

---
+++ utilies.cpp
@@ -0,0 +1,20 @@
+#include "utilies.h"
+#include "iostream"
+using namespace std;
+int Utilies::inputStringToInt() {
+    int inputNumber, errorCounter = 0;
+    string inputString;
+    while (true){
+        try {
+            cin >> inputString;
+            inputNumber = stoi(inputString);
+            break;
+        }
+        catch (...){
+            cerr << "Your input is not a number. It was: " << inputString << endl;
+            errorCounter++;
+            if (errorCounter>=20) { cerr<<"Many tries!" << endl;exit(-1);}
+        }
+    }
+    return inputNumber;
+}
---
+++ utilies.h
@@ -0,0 +1,11 @@
+#ifndef UTILIES_H
+#define UTILIES_H
+
+
+class Utilies
+{
+public:
+    static int inputStringToInt();
+};
+
+#endif // UTILIES_H
